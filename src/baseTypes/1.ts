bddd2aee301bdba995217170930f174d42ad7d83
Iryna-kondreskul <74777086+Iryna-kondreskul@users.noreply.github.com>
2023-10-25 00:07:03 +0300

    /

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,34 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+// let age = 50;
+// let name = 'Max';
+// let toggle = true;
+// let empty = null;
+// let notInitialize;
+// let callback = (a) => { return 100 + a };
 
-export {};
+// export {};
+
+// =======================================================
+
+let age:number = 50;
+let namee:string = 'Max';
+let toggle:boolean = true;
+let empty:null = null;
+let notInitialize:undefined = undefined;
+
+// або
+// const age = 50;
+// const name = 'Max';
+// const toggle = true;
+// const empty = null;
+// const notInitialize = undefined;
+
+function callback(num1:string, num2:string):string{
+    return `${num1} + ${num2}`
+}
+
+console.log(callback('100','a'))
+
+ export {};
+
